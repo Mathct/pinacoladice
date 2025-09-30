3c27c8bc01d8d5d79a92207fa000a74a975375ad
mathct <mathieu.chatrain@gmail.com>
2025-09-30 20:24:13 +0200

    copyright

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
- * pinacoladice implementation : © <Your name here> <Your email address here>
+ * pinacoladice implementation : © <Mathieu Chatrain> <mathieu.chatrain@gmail.com>
  *
  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
  * See http://en.boardgamearena.com/#!doc/Studio for more information.
